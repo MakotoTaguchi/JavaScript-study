b44549083ca39583c314c14982757b2be6c7237e
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2021-12-12 01:57:57 +0900

    main

---
+++ php/cross-origin/scripts/cross_main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var target = 'http://localhost';
+
+    document.getElementById('btn').addEventListener('click', function () {
+
+        document.getElementById('frame').contentWindow.postMessage(
+            document.getElementById('message').value, target);
+    }, false);
+
+    window.addEventListener('message', function (e) {
+        if (e.origin !== target) {
+            return;
+        }
+        console.log(e.data);
+    }, false);
+}, false);
